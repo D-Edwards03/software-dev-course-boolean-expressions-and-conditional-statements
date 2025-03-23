1ca5e265ad7199244f954c65371bcc363c315dfb
Demetrius Edwards <edwardsd03@outlook.com>
2025-03-23 16:38:53 -0500

    Complete

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -28,6 +28,8 @@ const readline = require('readline-sync');
 
 const hasTorch = true;
 const hasMap = false;
+const hasSword = true; //added item
+const hasCompass = false; //added item
 
 console.log("You see two paths: one leads to the mountains, the other to the village.");
 const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
@@ -40,6 +42,27 @@ if (choice === "mountains" && hasTorch) {
   console.log("You find your way to the village.");
 } else {
   console.log("You get lost and wander aimlessly.");
+}
+
+console.log("The night is coming soon and you stumble across a cave");
+const choice2 = readline.question("Do you enter the 'cave' or 'keep searching'? ");
+
+if (choice2 === "cave") {
+  if (hasTorch && hasSword) {
+    console.log("You defended yourself from the beast, and made shelter.");
+  } else if (hasTorch && !hasSword) {
+    console.log("You bought time to flea from the beast and escape the cave.");
+  } else if (!hasTorch && !hasSword) {
+    console.log("Sorry, you didn't make it out the cave.");
+  }
+} else if (choice2 === "keep searching") {
+  if (hasTorch && hasMap) {
+    console.log("You continue your search for shelter.");
+  } else if (!hasTorch && hasMap) {
+    console.log("You continue to wander in the darkness of the forest.");
+  } else if (hasMap || hasCompass) {
+    console.log("You decided to wait for sunlight.");
+  }
 }
 
 /* 
